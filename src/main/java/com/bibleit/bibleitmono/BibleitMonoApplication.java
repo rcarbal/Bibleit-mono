e9207479a55df0755c090ae8f598582e80290de8
rcarbal <rcarbaleq2@gmail.com>
2020-06-01 17:18:21 -0700

    initial commit

---
+++ src/main/java/com/bibleit/bibleitmono/BibleitMonoApplication.java
@@ -0,0 +1,13 @@
+package com.bibleit.bibleitmono;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibleitMonoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibleitMonoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bibleit/bibleitmono/BibleitMonoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bibleit.bibleitmono;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibleitMonoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
