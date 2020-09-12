dd091a4dcaadaea7f0cbd9f66506c34bc2518ba7
rcarbal <rcarbaleq2@gmail.com>
2020-09-12 08:21:15 -0700

    Check what class audio icon element contains.

--- src/main/resources/static/js/voice.js
+++ src/main/resources/static/js/voice.js
@@ -1,3 +1,23 @@
-alert("Voice file connected");
-
 let playVoiceVar = document.getElementById("voiceSelected");
+let playVoiceVal = playVoiceVar.dataset.voice;
+let voiceIcon = document.getElementById("voiceIcon");
+
+const OFF_ICON = "fa fa-volume-off fa-2x";
+const ON_ICON = "fa fa-volume-up fa-2x";
+
+// get child from preview div
+$(document).ready(function () {
+    $('#voiceSelected').on('click', function (event) {
+        changeVoiceIcon();
+    });
+});
+
+function changeVoiceIcon(){
+    
+    if (voiceIcon.classList.contains("fa-volume-off")){
+        console.log("Has fa fa-volume-off fa-2x")
+    }else{
+        console.log("Does not have class")
+    }
+}
+
